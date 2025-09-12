9b9d6b77775fe21b3d20f7cf25930569f466669b
Hill <hillkalola18@gmail.com>
2025-09-12 09:13:32 +0530

    initial Commit

---
+++ Basic/Area/areaOfCircle.py
@@ -0,0 +1,6 @@
+f = 3.14
+print("Enter the radius of the circle:")
+r = float(input())
+area = f * r * r
+print("Area of the circle is:", area)
+
---
+++ Basic/FerenhiteToCelcius/ferenheitToCelcius.py
@@ -0,0 +1,4 @@
+print("Enter temperature in Ferenhite: ")
+f = float(input())
+c = (f - 32) * 5 / 9
+print("Temperature in Celcius is:", c)
