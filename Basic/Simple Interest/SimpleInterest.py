5d623646d84d70b4b2ad261df2b6e903a217745b
Hill <hillkalola18@gmail.com>
2025-09-12 10:35:34 +0530

    Simple Interest

---
+++ Basic/Simple Interest/SimpleInterest.py
@@ -0,0 +1,9 @@
+print("Enter value of P: ");
+p = float(input());
+print("Enter value of R: ");
+r = float(input());
+print("Enter value of T: ");
+t = float(input());
+
+si = (p * r * t) / 100
+print("Simple Interest is:", si);
