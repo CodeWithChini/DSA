b01f1e9c5176908c626308070b556428651560d1
Hill <hillkalola18@gmail.com>
2025-09-16 08:04:21 +0530

    Tuples and Tuples Functions in python

---
+++ Basic/DataTypesInPython/Tuples.py
@@ -0,0 +1,10 @@
+#difference between tuples and list is once you created tuples it cannot be changed
+#Tuple are faster than list
+
+t = (10, 20, 30, "Hello")
+
+t = (100) # int
+print(type(t))
+t = (100,)  # tuple
+print(type(t))
+
---
+++ Basic/DataTypesInPython/TuplesFunction.py
@@ -0,0 +1,8 @@
+t = 10, 20, 30, 40, 10
+
+print(t[2])
+print(t[-1])
+print(t[1 : 4])
+print(len(t))
+print(t.count(10))
+print(t.index(30))
