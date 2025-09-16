25b17e90718b450804124193cf3f94d9b09a9277
Hill <hillkalola18@gmail.com>
2025-09-16 07:22:08 +0530

    Data Types in Python

---
+++ Basic/DataTypesInPython/Variable.py
@@ -0,0 +1,18 @@
+a = 10
+b = 18.9
+c = "Hello World"
+d = True
+e = ['hello', 10, False]
+f = (10, 20, 'hello')
+g = {10, 20, 'hello'}
+h = {'name': 'Hill', 'age': 20}
+
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'str'>
+print(type(d))  # <class 'bool'>
+print(type(e))  # <class 'list'>
+print(type(f))  # <class 'tuple'>
+print(type(g))  # <class 'set'>
+print(type(h))  # <class 'dict'>
+
