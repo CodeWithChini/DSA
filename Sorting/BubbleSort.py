20b678ea2cb7f6030edfbfe6021e4165593c3029
Hill <hillkalola18@gmail.com>
2026-01-13 10:30:10 +0530

    bubble sort in pyhton

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,18 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+    return arr
+
+myList = [64, 34, 45, 92, 17, 1, 29, 55]
+SortedList = bubbleSort(myList)
+print(SortedList)
