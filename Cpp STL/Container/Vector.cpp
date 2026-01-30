437b43a57554dace339c784a04e2d19a13fc4ff0
Hill <hillkalola18@gmail.com>
2026-01-30 05:35:23 +0530

    C++ STL vector functions

--- Cpp STL/Container/Vector.cpp
+++ Cpp STL/Container/Vector.cpp
@@ -28,5 +28,36 @@ int main()
     //size and capacity
     cout << vec.size() << endl; //3
     cout << vec.capacity() << endl; //4
+
+    vector<int> vec2 = {1, 2, 3, 4, 5};
+
+    //earse
+    vec2.erase(vec2.begin());
+    vec2.erase(vec2.begin() + 2); //delete a value at index 2
+
+    for(int val : vec2)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    vector<int> vec3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    // vec3.erase(vec3.begin() + 1, vec3.begin() + 9);
+
+    //insert
+    vec3.insert(vec3.begin() + 4 , 100);
+
+    //clear
+    vec3.clear();
+
+    for(int val : vec3)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    cout << vec3.size() << endl;
+    cout << vec3.capacity() << endl;
     return 0;
 }
